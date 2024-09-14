acf24e4bf8def4469dd90a7bf9dd5b96718f2824
Reece Peardon <peardon.reece@gmail.com>
2024-09-14 16:09:05 -0700

    passed in the books data to the books page to load all books within the data set

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {books} from './data'
 import Nav from './components/Nav'
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -11,7 +12,7 @@ function App() {
       <Nav />
       <Routes>
         <Route path='/' element={<Home />} />
-        {/*<Route path='/books' element={<Books books={books} />} />*/}
+        <Route path='/books' element={<Books books={books} />} />
       </Routes>
       <Footer />
     </div>
