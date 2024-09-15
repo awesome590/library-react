cf181d83d2a2cab68772e9748156b922d0194d99
Reece Peardon <peardon.reece@gmail.com>
2024-09-14 17:19:18 -0700

    deleted unused imports and made progress on the indiv book page

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import {books} from '../data'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
+import Rating from '../components/ui/Rating';
+import Price from '../components/ui/Price';
 
 function BookInfo({books}) {
   return (
@@ -8,7 +11,45 @@ function BookInfo({books}) {
         <div className="books__container">
           <div className="row">
             <div className="book__selected--top">
-              
+              <Link to='/books' className='book__link'>
+              <FontAwesomeIcon  icon='arrow-left' />
+              </Link>
+              <Link to='/books' className='book__link'>
+              <h2 className="book__selected--title--top">Books</h2>
+              </Link>
+            </div>
+            <div className="book__selected">
+              <figure className="book__selected--figure">
+                <img src="https://covers.openlibrary.org/b/id/8091016-L.jpg" alt="" className="book__selected--img" />
+              </figure>
+              <div className="book__selected--description">
+                <h2 className="book__selected--title">Crack</h2>
+                <Rating rating ='4.5'/>
+                <div className="book__selected--price">
+                  <Price originalPrice={50} salePrice={null}/>
+                </div>
+                <div className="book__summary">
+                  <div className="book__summary--title">Summary</div>
+                  <p className="book__summary--para">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere tenetur excepturi natus asperiores illo incidunt, dicta rerum minima beatae ad maiores cum, quas iste nihil voluptatibus perspiciatis recusandae, ipsum soluta.
+                  </p>
+                  <p className="book__summary--para">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere tenetur excepturi natus asperiores illo incidunt, dicta rerum minima beatae ad maiores cum, quas iste nihil voluptatibus perspiciatis recusandae, ipsum soluta.
+                  </p>
+                </div>
+                <button className="btn">
+                  Add to Cart
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="books__container">
+          <div className="row">
+            <div className="book__selected--top">
+              <h2 className="book__selected--title--top">
+                Recommended Books
+              </h2>
             </div>
           </div>
         </div>
