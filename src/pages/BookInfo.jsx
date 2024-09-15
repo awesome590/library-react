996fe64e0963fd3627f18fd1d140f6a643ceeab6
Reece Peardon <peardon.reece@gmail.com>
2024-09-14 17:37:40 -0700

    individual book page now shows selected book from books page, route fixed

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import Rating from '../components/ui/Rating';
 import Price from '../components/ui/Price';
 
-function BookInfo({books}) {
+const BookInfo = ({books}) => {
+  const {id} = useParams()
+  const book = books.find(book => +book.id === +id)
+  console.log(book)
   return (
     <div id="books__body">
       <main id="books__main">
@@ -20,13 +23,13 @@ function BookInfo({books}) {
             </div>
             <div className="book__selected">
               <figure className="book__selected--figure">
-                <img src="https://covers.openlibrary.org/b/id/8091016-L.jpg" alt="" className="book__selected--img" />
+                <img src={book.url} alt="" className="book__selected--img" />
               </figure>
               <div className="book__selected--description">
-                <h2 className="book__selected--title">Crack</h2>
-                <Rating rating ='4.5'/>
+                <h2 className="book__selected--title">{book.title}</h2>
+                <Rating rating={book.rating}/>
                 <div className="book__selected--price">
-                  <Price originalPrice={50} salePrice={null}/>
+                  <Price originalPrice={book.originalPrice} salePrice={book.salePrice}/>
                 </div>
                 <div className="book__summary">
                   <div className="book__summary--title">Summary</div>
