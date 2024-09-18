ef6a4a9c294d839e146974b2ded6738feb5dad26
Reece Peardon <peardon.reece@gmail.com>
2024-09-17 19:47:55 -0700

    finished building out cart page with dummy info

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import Books from './pages/Books';
 import BookInfo from './pages/BookInfo';
+import Cart from './pages/Cart';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/books' element={<Books books={books} />} />
         <Route path='/books/:id' element={<BookInfo books={books} />} />
+        <Route path='/cart' element={<Cart books={books} />} />
       </Routes>
       <Footer />
     </div>
