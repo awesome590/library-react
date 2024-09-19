cad201f0dafa2b56eaa576a574821e46e0e5706a
Reece Peardon <peardon.reece@gmail.com>
2024-09-19 16:53:54 -0700

    suggested code

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   const [cart, setCart] = useState([])
   
   function addToCart(book) {
-    setCart([...cart, book])
+    setCart(oldCart => [...oldCart, book])
   }
 
   useEffect(() => {
